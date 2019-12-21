0abf5cb2516f8c7a0ac2a72df1dddf697b0b9f57
Nay <emilydevoos@gmail.com>
2019-12-21 23:58:24 +0100

    Variables

---
+++ 03-variables.js
@@ -0,0 +1,9 @@
+// Variables let you save information in the PC's RAM (memory).
+
+var uwu = 1; // Create a variable with name: uwu, and value: 1
+
+console.log(uwu); // Writes the value of uwu to the screen (1)
+
+uwu = 50; // Change value of uwu variable to: 50
+
+console.log(uwu) // Writes the value of uwu (now 50)
