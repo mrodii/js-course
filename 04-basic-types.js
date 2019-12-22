980f588860f09724716aa5398c65b51df44023a2
Nay <emilydevoos@gmail.com>
2019-12-22 01:30:52 +0100

    Basic types

---
+++ 04-basic-types.js
@@ -0,0 +1,28 @@
+// Data (for example variables) can be of different types
+
+let myNumber = 5; // Type number
+let myOtherNumber = 12.47; // Type number
+
+let myText = 'uwu'; // Type string
+let myOtherText = "u.u"; // Type string
+
+let myBoolean = true; // Type boolean (can be true or false)
+let myOtherBoolean = false; // Type boolean
+
+
+// Using typeof to see a type
+console.log(typeof 125); // -> number
+console.log(typeof 'AYAYA'); // -> string
+console.log(typeof false); // -> boolean
+
+// Video games are a good way to explain things
+// So let's make a video game example
+
+// You go for a game of League, game would start with these values:
+let player = 'Odi';
+let hp = 850;
+let isAlive = true;
+
+console.log(`Player name: ${player}`); // -> Player name: Odi
+console.log(`HP: ${hp}`); // -> HP: 850
+console.log(`Alive: ${isAlive}`); // -> Alive: true
