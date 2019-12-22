ec1bc9b6f1e383c2f40ce693475a7da26a482dae
Nay <emilydevoos@gmail.com>
2019-12-22 05:22:47 +0100

    Exercises

---
+++ exercises/ex1.js
@@ -0,0 +1,8 @@
+// Exercice 1
+
+let first = true;
+let second = false;
+let negative = -50;
+let decimal = 2.58;
+let decimalNegative = -5.21;
+let iLoveYou = 'I love you Odiii u.u';
---
+++ exercises/ex2.js
@@ -0,0 +1,14 @@
+let hp = 500;
+console.log(hp);
+
+hp += 10;
+hp -= 5;
+console.log(hp);
+
+hp *= 5000;
+console.log(hp);
+
+hp /= 32.114;
+console.log(hp)
+
+console.log('js is op uwu');
