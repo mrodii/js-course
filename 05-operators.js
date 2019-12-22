dcf29393c72800bbb316e316f971906e291fb1c6
Nay <emilydevoos@gmail.com>
2019-12-22 05:22:42 +0100

    Base operators

---
+++ 05-operators.js
@@ -0,0 +1,12 @@
+// There are multiple operators in JS, including these:
+
+// + to add
+// - to subtract
+// * to multiply
+// / to divide
+
+let num = 5; // create variable num with value 5
+num += 2; // add 2 to num
+num -= 2.4 // subtract 2.4 from num
+num *= 5; // multiply num by 5
+num /= 2; // divide num by 2
